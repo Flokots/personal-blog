b3ae6cf38daeb8dfc90a244b95c565eb8547162c
FloKots <florencekotohoyoh@gmail.com>
2022-05-16 02:47:52 +0300

    display quote

--- app/request.py
+++ app/request.py
@@ -1,4 +1,34 @@
 from app import app
+import urllib.request, json
+from .models import quote
+
+Quote = quote.Quote
 
 # Getting api key
-api_key = app.config['QUOTES_BASE_URL']
+base_url = app.config['QUOTES_BASE_URL']
+
+
+def get_quote():
+  '''
+  Function that gets the json response to our url request
+  '''
+  get_quote_url= base_url
+
+  with urllib.request.urlopen(get_quote_url) as url:
+    get_quote_data = url.read()
+    quote_response = json.loads(get_quote_data)
+    if quote_response:
+      author = quote_response.get("author")
+      id = quote_response.get("id")
+      quote = quote_response.get("quote")
+      permalink = quote_response.get("permalink")
+
+      quote_object = Quote(author, id, quote, permalink)
+  return quote_object
+
+
+
+ 
+
+
+
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_quote
 
 
 @app.route('/')
@@ -7,7 +8,10 @@ def index():
   '''
   View root page function that returns the index page and its data
   '''
-  return render_template('index.html')
+  
+  quote = get_quote()
+  print(quote)
+  return render_template('index.html', quote=quote)
 
   
 @app.route('/blog/<int:id>')
