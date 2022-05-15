f2d1433f94e13b30eed72e04886155643da70691
FloKots <florencekotohoyoh@gmail.com>
2022-05-15 12:57:28 +0300

    add test_init quote class

--- app/quote_test.py
+++ app/quote_test.py
@@ -18,5 +18,17 @@ class QuoteTest(unittest.TestCase):
     self.assertTrue(isinstance(self.new_quote, Quote))
 
 
+  def test_init(self):
+    '''
+    test_init test case to test if the object is initialized properly.
+    '''
+    self.assertEqual(self.new_quote.author, "Bill Gates")
+    self.assertEqual(self.new_quote.id, 6)
+    self.assertEqual(self.new_quote.quote, "Measuring programming progress by lines of code is like measuring aircraft building progress by weight.")
+    self.assertEqual(self.new_quote.permalink, "http://quotes.stormconsultancy.co.uk/quotes/6")
+  
+  
+
+
 if __name__ == '__main__':
   unittest.main()
