e6d02621e491d64bf1860492e41ebb9e5a108493
FloKots <florencekotohoyoh@gmail.com>
2022-05-16 14:45:58 +0300

    add about, contact pages

--- app/main/views.py
+++ app/main/views.py
@@ -16,7 +16,25 @@ def index():
   title = "Personal Blog"
   return render_template('index.html', quote=quote, title=title)
 
-  
+
+@main.route('/about')
+def about():
+  '''
+  View page function that returns the about page and its data
+  '''
+  title = "About"
+  return render_template('about.html', title=title)
+
+
+@main.route('/contact')
+def contact():
+  '''
+  View page function that returns the contact page and its data
+  '''
+  title = "Contact"
+  return render_template('contact.html', title=title)
+
+
 @main.route('/blog/<int:id>')
 def blog(id):
   '''
