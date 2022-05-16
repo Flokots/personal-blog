7d76987153e28e7a61308e5fcfbf62d4970188e7
FloKots <florencekotohoyoh@gmail.com>
2022-05-16 09:54:24 +0300

    add pass-secure column

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 
 
@@ -19,7 +20,19 @@ class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(255), index=True)
   role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+  pass_secure = db.Column(db.String(255))
 
+  @property
+  def password(self):
+    raise AttributeError('You cannot read the password attribute')
+
+  
+  @password.setter
+  def password(self, password):
+    self.pass_secure = generate_password_hash(password)
+
+  def verify_password(self, password):
+    return check_password_hash(self.pass_secure, password)
 
   def __repr__(self):
     return f'{self.username}'
@@ -32,6 +45,7 @@ class Role(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(255))
   users = db.relationship('User', backref='role', lazy='dynamic')
+  
 
 
   def __repr__(self):
