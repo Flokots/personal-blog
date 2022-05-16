f2d1c5b31c7e164e95da5491d5e0e3a914042308
FloKots <florencekotohoyoh@gmail.com>
2022-05-16 11:24:00 +0300

    configure user model

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,1 +1,4 @@
-from flask_wtf import Flask
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
+from ..models import User
