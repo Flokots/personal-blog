329808e70b2d2bf46d979d38b8c9a5d46a8c4073
FloKots <florencekotohoyoh@gmail.com>
2022-05-16 21:08:02 +0300

    upload images

--- config.py
+++ config.py
@@ -4,9 +4,10 @@ class Config:
   '''
   General configuration parent class
   '''
-  QUOTES_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
-  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://flo:flo@localhost:5433/personal_blog'
-  SECRET_KEY = os.environ.get('SECRET_KEY')
+  QUOTES_BASE_URL='http://quotes.stormconsultancy.co.uk/random.json'
+  SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://flo:flo@localhost:5433/personal_blog'
+  SECRET_KEY=os.environ.get('SECRET_KEY')
+  UPLOADED_PHOTOS_DEST='app/static/photos'
 
 
 class ProdConfig(Config):
