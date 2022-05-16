52eb34803d40902a4679ff36d67af889215d2f48
FloKots <florencekotohoyoh@gmail.com>
2022-05-16 14:57:26 +0300

    add profile route

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,9 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, abort
 from flask_login import login_required
 
 from . import main
 from ..requests import get_quote
-from ..models import Quote
+from ..models import Quote, User
 
 
 @main.route('/')
@@ -41,5 +41,15 @@ def blog(id):
   View blog page function that returns the blog details page and its data
   '''
   return render_template('blog.html', id=id)
+
+
+@main.route('/user/<uname>')
+def profile(uname):
+  user = User.query.filter_by(username=uname).first()
+
+  if user is None:
+    abort(404)
+  
+  return render_template('profile/profile.html', user=user)
 
 
