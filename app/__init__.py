58cd7d2b2117edb71934db39300c311d768145a0
FloKots <florencekotohoyoh@gmail.com>
2022-05-17 06:55:14 +0300

    add simplemde

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet, configure_uploads, IMAGES
 from flask_mail import Mail
+from flask_simplemde import SimpleMDE
 
 from config import config_options
 
-bootstrap = Bootstrap()
-db = SQLAlchemy()
-mail = Mail()
+bootstrap=Bootstrap()
+db=SQLAlchemy()
+mail=Mail()
+simple=SimpleMDE()
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -28,6 +30,7 @@ def create_app(config_name):
   db.init_app(app)
   login_manager.init_app(app)
   mail.init_app(app)
+  simple.init_app(app)
 
   # Registering the blueprint
   from .main import main as main_blueprint
