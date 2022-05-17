37656213490406eb5cbd9e3ca8a1ca7c84667ced
FloKots <florencekotohoyoh@gmail.com>
2022-05-17 12:38:37 +0300

    update requirements

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ class ProdConfig(Config):
   Args:
     Config: The parent configuration class with General Configuration settings
   '''
-  SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+  SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace("://", "ql://", 1)
 
 
 class DevConfig(Config):
