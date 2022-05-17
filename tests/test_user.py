69457f0e846743e6314300036c761c3ba37e4760
FloKots <florencekotohoyoh@gmail.com>
2022-05-17 11:46:35 +0300

    add test user test cases

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,10 +1,14 @@
+import email
 import unittest
 from app.models import User
 
 class UserModelTest(unittest.TestCase):
 
   def setUp(self):
-    self.new_user = User(password = 'banana')
+    self.new_user = User(id=12345, password= 'banana', username='ndizi', email='ndizi@gmail.com', bio='hey world', profile_pic_path='/static/photos/star.jpg')
+  
+  def tearDown(self):
+    User.query.delete()
 
   def test_password_setter(self):
     self.assertTrue(self.new_user.pass_secure is not None)
@@ -15,3 +19,13 @@ class UserModelTest(unittest.TestCase):
 
   def test_password_verification(self):
     self.assertTrue(self.new_user.verify_password('banana'))
+
+  def test_init(self):
+    '''
+    test_init test case to test if the object is initialized properly.
+    '''
+    self.assertEqual(self.new_user.id, 12345)
+    self.assertEqual(self.new_user.username, 'ndizi')
+    self.assertEqual(self.new_user.email, 'ndizi@gmail.com')
+    self.assertEqual(self.new_user.bio, 'hey world')
+    self.assertEqual(self.new_user.profile_pic_path, '/static/photos/star.jpg')
